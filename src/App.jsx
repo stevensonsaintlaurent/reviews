3cad90399da8ff16cc67eb65304f4977fea008be
Stevenson <stevensonstlaurent08@gmail.com>
2026-03-31 23:02:21 -0700

    updating

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,19 @@ import people from "./data";
 const App = () => {
   const [index, setIndex] = useState(0);
   const { name, job, image, text } = people[index];
+  const prevPerson = () => {
+    setIndex((currentIndex) => {
+      const newIndex = currentIndex - 1;
+      return newIndex;
+    });
+  };
+
+  const nextPerson = () => {
+    setIndex((currentIndex) => {
+      const newIndex = currentIndex + 1;
+      return newIndex;
+    });
+  };
   console.log(name);
   return (
     <main>
@@ -18,6 +31,14 @@ const App = () => {
         <h4 className="author">{name}</h4>
         <p className="job">{job}</p>
         <p className="info">{text}</p>
+        <div className="btn-container">
+          <button className="prev-btn" onClick={prevPerson}>
+            <FaChevronLeft />
+          </button>
+          <button className="next-btn" onClick={nextPerson}>
+            <FaChevronRight />
+          </button>
+        </div>
       </article>
     </main>
   );
